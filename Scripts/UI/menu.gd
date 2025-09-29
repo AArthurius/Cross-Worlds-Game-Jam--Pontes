a145a3d4df2f195cb58da19b674877a4587bc40b
AArthurius <arthurpennarey@gmail.com>
2025-09-29 17:40:41 -0300

    Add main menu UI and font, update scenes
    Introduced a new main menu scene and script with Play, Config, and Exit buttons using the PixelOperator font. Updated Dialog UI to use the new font and increased font size. Refactored NPC animations in NPC.tscn, moved SheriffsBox.tscn to GameObjects, and removed the obsolete deletar.tscn scene.

---
+++ Scripts/UI/menu.gd
@@ -0,0 +1,13 @@
+extends Control
+
+const MAIN = preload("uid://3o1b3o2tyyeg")
+
+
+func Play_pressed() -> void:
+	get_tree().change_scene_to_packed(MAIN)
+
+func Config_pressed() -> void:
+	pass # Replace with function body.
+
+func Exit_pressed() -> void:
+	pass # Replace with function body.
