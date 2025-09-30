4a53429c23aef2fdd56c4d2509ce0cdb729c4964
Itallo Casimiro Felix <itallocasimiro.doc@gmail.com>
2025-09-30 20:29:35 -0300

    Novos NPCs adicionados
    - Novos NPCs adicionados e seus resources de documento pessoal;
    - Mudança na seleção de país no documento pessoal de string para um enum;

--- Scripts/ResourcesScripts/Documents/NPCPersonalDocument.gd
+++ Scripts/ResourcesScripts/Documents/NPCPersonalDocument.gd
@@ -1,9 +1,10 @@
 extends Resource
 
-enum CitadelWorks {None, Saloon, Sheriff, Hotel, PrisonGovernment, Bridge}
+enum CitadelWorks {None, Saloon, Sheriff, Hotel, Prison_Government, Bridge}
+enum Countries {Brazil, United_Kingdoom, Germany, Portugal}
 
 @export var npcProfileImage: Texture2D
 @export var npcName: String
 @export var npcAge: int
-@export var npcCountry: String
+@export var npcCountry: Countries
 @export var npcCitadelWork: CitadelWorks
